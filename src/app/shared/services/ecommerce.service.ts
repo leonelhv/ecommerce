e59af6431168b8b020a4cee62cb26d29460138bd
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-02-01 20:31:24 -0500

    add documentation on ecommerce service

--- src/app/shared/services/ecommerce.service.ts
+++ src/app/shared/services/ecommerce.service.ts
@@ -19,23 +19,25 @@ export class EcommerceService {
   constructor(private firestore: Firestore) {
     this.cartUser = this.getCartLocalStorage();
   }
-
+  //Obtiene el carrito desde el localstorage
   getCartLocalStorage() {
     const getCartLocalStorage = JSON.parse(
       localStorage.getItem('cart') || '[]'
     );
     return getCartLocalStorage;
   }
-
+  //Guarda el carrito en el localstorage
   saveCartLocalStorage(cart: CartUser[]) {
     localStorage.setItem('cart', JSON.stringify(cart));
   }
 
+  //Agrega un producto a firebase
   addproduct(Product: Partial<Product>) {
     const productRef = collection(this.firestore, 'products');
     return addDoc(productRef, Product);
   }
 
+  //Obtiene todos los productos
   getProducts(): Observable<Product[]> {
     const productRef = collection(this.firestore, 'products');
     return collectionData(productRef, { idField: 'id' }) as Observable<
@@ -43,11 +45,13 @@ export class EcommerceService {
     >;
   }
 
+  //Elimina el producto en firebase
   deleteProduct(Product: Product) {
     const productRef = doc(this.firestore, `products/${Product.id}`);
     return deleteDoc(productRef);
   }
 
+  //Para actualizar el producto aun no implementado en el dashboard
   updateProduct(id: string, Product: Product) {
     const productRef = doc(this.firestore, `products/${id}`);
     return updateDoc(productRef, { ...Product });
