559acf96a4ed8c67c0ac2392a31d7db79d48d37f
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-02-02 15:18:14 -0500

    dashboard responsive

--- src/app/dashboard/components/header/header.component.ts
+++ src/app/dashboard/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { user } from 'src/app/interfaces/auth.interface';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -10,13 +10,20 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class HeaderComponent {
   user!: user;
+  @Output() eventHamburger = new EventEmitter<void>();
+
   constructor(private authService: AuthService, private router: Router) {
     this.user = JSON.parse(localStorage.getItem('user')!);
   }
+
   onLogout() {
     this.authService.logout().then((res) => {
       localStorage.removeItem('user');
       this.router.navigate(['auth/login']);
     });
+  }
+
+  onShowSidebar() {
+    this.eventHamburger.emit();
   }
 }
--- src/app/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/dashboard/components/sidebar/sidebar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  @Output() eventClickLink = new EventEmitter<void>();
 
+  onEventClickLink() {
+    this.eventClickLink.emit();
+  }
 }
